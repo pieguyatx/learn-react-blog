ef104868d38f7185595df7a36036a1922081426a
Pius Wong <pius@pioslabs.com>
2021-06-27 18:49:02 -0500

    Connect post list component to app

---
+++ src/components/PostList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PostList extends React.Component {
+    render() {
+        return (
+            <div>Post list goes here</div>
+        );
+    }
+};
+
+export default PostList;
