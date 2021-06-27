10afc4f8a70cbfd6afbac6b0c46f8f9e63715617
Pius Wong <pius@pioslabs.com>
2021-06-27 18:44:46 -0500

    Set up React/Redux boilerplate with dummy vars

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="ui container">App goes here</div>
+    );
+};
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+    // dummy variable to get rid of error messages in the beginning until you can fill in real stuff
+    dummyKey: () => 999 
+});
