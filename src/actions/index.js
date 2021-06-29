fdb19660319de97b39d0b196cfe2ac8543ce6eb7
Pius Wong <pius@pioslabs.com>
2021-06-29 18:29:21 -0500

    Prepare async action creator using redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,18 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
-    // bad approach!!! Breaks rules of Redux and action creators...
-    // will give error message: "Actions must be plain objects..."
-    // can test out babeljs.io to see what it transpiles to... 
-    // async/await syntax changes the function when it transpiles; no longer returning plan JS object.
+// redux-thunk let's us return a function
+// this function returns another function
+export const fetchPosts = () => async dispatch => { // getState can be second arg next to dispatch
+    // only concerned with what gets returned from the action creator function, not the inner one
     const response = await jsonPlaceholder.get('/posts');
 
+    // don't need to return an action in an inner function here, just use dispatch
+    dispatch({ type: 'FETCH_POSTS', payload: response });  
+};
+
+// still fine
+export const selectPost = () => {
     return {
-        type: 'FETCH_POSTS',
-        payload: response
-    };
+        type: 'SELECT_POST'
+    }
 };
