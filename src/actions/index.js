240c337d979115da42fe8afcf7ee3d623c9e63dd
Pius Wong <pius@pioslabs.com>
2021-06-29 17:38:53 -0500

    Set up axios api call to get json, using bad method

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,12 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = async () => {
+    // bad approach!!! Breaks rules of Redux and action creators...
+    // will give error message: "Actions must be plain objects..."
+    const response = await jsonPlaceholder.get('/posts');
+
     return {
-        type: 'FETCH_POSTS'
+        type: 'FETCH_POSTS',
+        payload: response
     };
 };
