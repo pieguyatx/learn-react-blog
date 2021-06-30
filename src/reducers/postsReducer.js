63bc20430c24d4607637cc91d624fb9682d3ce4e
Pius Wong <pius@pioslabs.com>
2021-06-30 16:57:01 -0500

    Posts reducer returns payload

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,7 +1,7 @@
 // Reducer produces 'state' data, based *only* on previous 'state' data inputs and an 'action'
 // ...so don't do any API requests, pulling values from DOM, etc...
 // This keeps the reducer "pure" and only basing things off the state and action
-export default () => {
+export default (state = [], action) => {
 
     // A reducer always needs a return statement of something not "undefined"
     // ***Should not "mutate" the input state data, or change the old data; 
@@ -23,5 +23,19 @@ export default () => {
     //    Objects:  {...state, key:'blah'}, 
     //              {...state, newProp:value}, 
     //              {...state, oldProp:undefined} / _.omit(state,'oldProp') <-- better to use Lo dash library
-    return 123;
+
+    if (action.type === 'FETCH_POSTS') {
+        return action.payload;
+    }
+
+    return state;
+    // // or use switch statement:
+    // switch ( action.type ) {
+    //     case 'FETCH_POSTS':
+    //         // what to do
+    //         return action.payload;
+    //     default:
+    //         return state;
+    // }
+
 }
